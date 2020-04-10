f1131222529102c1d070029b93720a63ee1fcaed
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2020-04-10 14:23:52 +0300

    Initial commit

---
+++ src/assets/js/app.js
@@ -0,0 +1,14 @@
+// Подключенные библиотеки! Типа внизу linki
+
+//= ../../../node_modules/bootstrap/js/dist/util.js
+//= ../../../node_modules/bootstrap/js/dist/modal.js
+//= ../../../node_modules/slick-carousel/slick/slick.js
+
+
+
+
+
+// // Самописный КОД!
+// $(function(){
+//     //= components/slick.js
+// })
---
+++ src/assets/js/components/slick.js
@@ -0,0 +1,24 @@
+// var portfolio = $("#portfolio");
+
+// portfolio.slick({
+//   arrows: false,
+//   dots: true,
+//   speed: 800,
+//   slidesToShow: 2,
+//   slidesToScroll: 2
+// });
+
+
+
+// $("#portfolio-arrow-next").on("click", function (event) {
+//   event.preventDefault();
+
+//   portfolio.slick('slickNext');
+// });
+
+// $("#portfolio-arrow-prev").on("click", function (event) {
+//   event.preventDefault();
+
+
+//   portfolio.slick('slickPrev');
+// });
